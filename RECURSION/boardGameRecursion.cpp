f0e03cc4804c5d9a93b89e13934df21c6c81a16e
VivekWesley <vivekjohnwesley@gmail.com>
2021-01-28 10:45:20 +0530

    ADD board game using recursion

---
+++ RECURSION/boardGameRecursion.cpp
@@ -0,0 +1,179 @@
+// DATE: 2K21
+// ______________________________________
+
+// sample output:
+// 4
+
+// #include <iostream>
+// using namespace std;
+
+// int countPath(int s, int e)
+// {
+//     if (s == e)
+//     {
+//         return 1;
+//     }
+
+//     if (s > e)
+//     {
+//         return 0;
+//     }
+
+//     int count = 0;
+//     for (int i = 1; i <= 6; i++)
+//     {
+//         count += countPath(s + i, e);
+//     }
+//     return count;
+// }
+// int main()
+// {
+//     cout << countPath(0, 3) << endl;
+//     return 0;
+// }
+
+// #1
+// #include <iostream>
+// using namespace std;
+
+// int countPath(int s, int e)
+// {
+//     if (s == e)
+//     {
+//         return 1;
+//     }
+//     if (s > e)
+//     {
+//         return 0;
+//     }
+
+//     int count = 0;
+//     for (int i = 1; i <= 6; i++)
+//     {
+//         count += countPath(s + i, e);
+//     }
+//     return count;
+// }
+
+// int main()
+// {
+//     cout << countPath(0, 3);
+//     return 0;
+// }
+
+// #2
+// #include <iostream>
+// using namespace std;
+
+// int countPath(int s, int e)
+// {
+//     if (s == e)
+//     {
+//         return 1;
+//     }
+//     if (s > e)
+//     {
+//         return 0;
+//     }
+
+//     int count = 0;
+//     for (int i = 1; i <= 6; i++)
+//     {
+//         count += countPath(s + i, e);
+//     }
+//     return count;
+// }
+
+// int main()
+// {
+//     cout << countPath(0, 3);
+//     return 0;
+// }
+
+// #3
+// #include <iostream>
+// using namespace std;
+
+// int countPath(int s, int e)
+// {
+//     if (s == e)
+//     {
+//         return 1;
+//     }
+//     if (s > e)
+//     {
+//         return 0;
+//     }
+
+//     int count = 0;
+//     for (int i = 1; i <= 6; i++)
+//     {
+//         count += countPath(s + i, e);
+//     }
+//     return count;
+// }
+
+// int main()
+// {
+//     cout << countPath(0, 3) << endl;
+//     return 0;
+// }
+
+// #4
+// #include <iostream>
+// using namespace std;
+
+// int countPath(int s, int e)
+// {
+//     if (s == e)
+//     {
+//         return 1;
+//     }
+//     if (s > e)
+//     {
+//         return 0;
+//     }
+
+//     int count = 0;
+//     for (int i = 1; i <= 6; i++)
+//     {
+//         count += countPath(s + i, e);
+//     }
+//     return count;
+// }
+
+// int main()
+// {
+//     cout << countPath(0, 3) << endl;
+//     return 0;
+// }
+
+// #5
+#include <iostream>
+using namespace std;
+int countPath(int s, int e)
+{
+    if (s == e)
+    {
+        return 1;
+    }
+
+    if (s > e)
+    {
+        return 0;
+    }
+
+    int count = 0;
+
+    for (int i = 1; i < 6; i++)
+    {
+        count += countPath(s + i, e);
+    }
+    return count;
+}
+
+int main()
+{
+    cout << countPath(0, 3) << endl;
+    return 0;
+}
