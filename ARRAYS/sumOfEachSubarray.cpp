bc1ba0e3e2a127d5f61e2d8c684448cf533968f9
VivekWesley <vivekjohnwesley@gmail.com>
2021-03-20 20:30:13 +0530

    ADD print sum of all the subarrays

---
+++ ARRAYS/sumOfEachSubarray.cpp
@@ -0,0 +1,188 @@
+// DATE: MAR 20 2K21
+// print sum of each subarray of the given array
+
+// sample output:
+// enter array size:
+// 3
+// enter array elements:
+// 1 2 2
+// 1
+// 3
+// 5
+// 2
+// 4
+// 2
+// __________________________________________________________________________________________
+
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int n;
+
+//     cout << "enter array size: " << endl;
+//     cin >> n;
+
+//     int arr[n];
+//     cout << "enter array elements: " << endl;
+//     for (int i = 0; i < n; i++)
+//     {
+//         cin >> arr[i];
+//     }
+
+//     int sum = 0;
+//     for (int i = 0; i < n; i++)
+//     {
+//         sum = 0;
+//         for (int j = i; j < n; j++)
+//         {
+//             sum += arr[j];
+//             cout << sum << endl;
+//         }
+//     }
+
+//     return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int n;
+//     cout << "enter array size: " << endl;
+//     cin >> n;
+
+//     int arr[n];
+//     for (int i = 0; i < n; i++)
+//     {
+//         cin >> arr[i];
+//     }
+
+//     int sum = 0;
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         int sum = 0;
+//         for (int j = i; j < n; j++)
+//         {
+//             sum += arr[j];
+//             cout << sum << endl;
+//         }
+//     }
+
+//     return 0;
+// }
+
+// #2
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int n;
+//     cout << "enter array size: " << endl;
+//     cin >> n;
+//     int arr[n];
+//     cout << "enter array elements: " << endl;
+//     for (int i = 0; i < n; i++)
+//     {
+//         cin >> arr[i];
+//     }
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         int currentSum = 0;
+//         for (int j = i; j < n; j++)
+//         {
+//             currentSum = currentSum + arr[j];
+//             cout << currentSum << endl;
+//         }
+//     }
+//     return 0;
+// }
+
+// #3
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int n;
+//     cout << "enter array size: " << endl;
+//     cin >> n;
+//     int arr[n];
+//     cout << "enter array elements: " << endl;
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         cin >> arr[i];
+//     }
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         int currentSum = 0;
+//         for (int j = i; j < n; j++)
+//         {
+//             currentSum = currentSum + arr[j];
+//             cout << currentSum << endl;
+//         }
+//     }
+//     return 0;
+// }
+
+// #4
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int n;
+//     cout << "enter array size: " << endl;
+//     cin >> n;
+//     int arr[n];
+
+//     cout << "enter array elements: " << endl;
+//     for (int i = 0; i < n; i++)
+//     {
+//         cin >> arr[i];
+//     }
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         int currentSum = 0;
+//         for (int j = i; j < n; j++)
+//         {
+//             currentSum = currentSum + arr[j];
+//             cout << currentSum << endl;
+//         }
+//     }
+//     return 0;
+// }
+
+// #5
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "enter array size: " << endl;
+    cin >> n;
+    int arr[n];
+    cout << "enter array elements: " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        int currentSum = 0;
+        for (int j = i; j < n; j++)
+        {
+            currentSum = currentSum + arr[j];
+            cout << currentSum << endl;
+        }
+    }
+    return 0;
+}
