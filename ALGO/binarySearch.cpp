756a603e7246fc600d848a132561d87752219aa9
VivekWesley <vivekjohnwesley@gmail.com>
2021-04-17 21:57:52 +0530

    ADD binary search practice

--- ALGO/binarySearch.cpp
+++ ALGO/binarySearch.cpp
@@ -59,6 +59,52 @@
 // }
 
 // #2
+// #include <iostream>
+// using namespace std;
+
+// int binarySearch(int arr[], int n, int target)
+// {
+//     int L = 0;
+//     int R = n - 1;
+
+//     while (L <= R)
+//     {
+//         int mid = L + (R - L) / 2;
+//         if (arr[mid] == target)
+//         {
+//             return mid;
+//         }
+//         else if (target < arr[mid])
+//         {
+//             R = mid - 1;
+//         }
+//         else
+//         {
+//             L = mid + 1;
+//         }
+//     }
+//     return -1;
+// }
+
+// int main()
+// {
+//     int n;
+//     cout << "enter array size: " << endl;
+//     cin >> n;
+//     int arr[n];
+//     cout << "enter array elements: " << endl;
+//     for (int i = 0; i < n; i++)
+//     {
+//         cin >> arr[i];
+//     }
+//     int target;
+//     cout << "enter target element:" << endl;
+//     cin >> target;
+//     cout << "element fpund at index: " << binarySearch(arr, n, target) << endl;
+//     return 0;
+// }
+
+// #3
 #include <iostream>
 using namespace std;
 
@@ -91,15 +137,19 @@ int main()
     int n;
     cout << "enter array size: " << endl;
     cin >> n;
+
     int arr[n];
     cout << "enter array elements: " << endl;
     for (int i = 0; i < n; i++)
     {
         cin >> arr[i];
     }
+
     int target;
-    cout << "enter target element:" << endl;
+    cout << "enter target element: " << endl;
     cin >> target;
-    cout << "element fpund at index: " << binarySearch(arr, n, target) << endl;
+
+    cout << target + " is found at INDEX: " << binarySearch(arr, n, target) << endl;
+
     return 0;
 }
