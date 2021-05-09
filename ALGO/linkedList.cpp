4e456fd913c884578fdb7c1d0e8568a113873cec
VivekWesley <vivekjohnwesley@gmail.com>
2021-05-09 23:41:32 +0530

    ADD linked list practice

--- ALGO/linkedList.cpp
+++ ALGO/linkedList.cpp
@@ -476,12 +476,72 @@
 // }
 
 // #8
+// #include <iostream>
+// using namespace std;
+
+// class node
+// {
+
+// public:
+//     int data;
+//     node *next;
+
+//     node(int val)
+//     {
+//         data = val;
+//         next = NULL;
+//     }
+// };
+
+// void insertAtTail(node *&head, int val)
+// {
+//     node *n = new node(val);
+
+//     if (head == NULL)
+//     {
+//         head = n;
+//         return;
+//     }
+
+//     node *temp = head;
+//     while (temp->next != NULL)
+//     {
+//         temp = temp->next;
+//     }
+//     temp->next = n;
+// }
+
+// void display(node *head)
+// {
+//     node *temp = head;
+//     while (temp != NULL)
+//     {
+//         cout << temp->data << "->";
+//         temp = temp->next;
+//     }
+//     cout << "NULL" << endl;
+// }
+
+// int main()
+// {
+//     node *head = NULL;
+//     insertAtTail(head, 2);
+//     insertAtTail(head, 4);
+//     insertAtTail(head, 6);
+//     insertAtTail(head, 8);
+//     insertAtTail(head, 10);
+
+//     display(head);
+
+//     return 0;
+// }
+
+// #9
 #include <iostream>
 using namespace std;
 
 class node
 {
-
 public:
     int data;
     node *next;
@@ -525,13 +585,13 @@ void display(node *head)
 int main()
 {
     node *head = NULL;
+    insertAtTail(head, 1);
     insertAtTail(head, 2);
+    insertAtTail(head, 3);
     insertAtTail(head, 4);
+    insertAtTail(head, 5);
     insertAtTail(head, 6);
-    insertAtTail(head, 8);
-    insertAtTail(head, 10);
 
     display(head);
-
     return 0;
 }
