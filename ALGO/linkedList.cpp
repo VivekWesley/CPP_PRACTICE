6a2524f90d3ba7845e17cf07c96109115a49dab7
VivekWesley <vivekjohnwesley@gmail.com>
2021-05-12 23:33:17 +0530

    ADD linked list practice

--- ALGO/linkedList.cpp
+++ ALGO/linkedList.cpp
@@ -660,6 +660,66 @@
 // }
 
 // #11
+// #include <iostream>
+// using namespace std;
+
+// class node
+// {
+// public:
+//     int data;
+//     node *next;
+//     node(int val)
+//     {
+//         data = val;
+//         next = NULL;
+//     }
+// };
+
+// void insertAtTail(node *&head, int val)
+// {
+//     node *n = new node(val);
+
+//     if (head == NULL)
+//     {
+//         head = n;
+//         return;
+//     }
+
+//     node *temp = head;
+//     while (temp->next != NULL)
+//     {
+//         temp = temp->next;
+//     }
+//     temp->next = n;
+// }
+
+// void display(node *head)
+// {
+//     node *temp = head;
+//     while (temp != NULL)
+//     {
+//         cout << temp->data << "->";
+//         temp = temp->next;
+//     }
+//     cout << "NULL" << endl;
+// }
+
+// int main()
+// {
+//     node *head = NULL;
+
+//     insertAtTail(head, 1);
+//     insertAtTail(head, 3);
+//     insertAtTail(head, 5);
+//     insertAtTail(head, 7);
+//     insertAtTail(head, 10);
+
+//     display(head);
+
+//     return 0;
+// }
+
+// #12
 #include <iostream>
 using namespace std;
 
@@ -668,6 +728,7 @@ class node
 public:
     int data;
     node *next;
+
     node(int val)
     {
         data = val;
@@ -678,7 +739,6 @@ public:
 void insertAtTail(node *&head, int val)
 {
     node *n = new node(val);
-
     if (head == NULL)
     {
         head = n;
@@ -686,6 +746,7 @@ void insertAtTail(node *&head, int val)
     }
 
     node *temp = head;
+
     while (temp->next != NULL)
     {
         temp = temp->next;
@@ -707,11 +768,12 @@ void display(node *head)
 int main()
 {
     node *head = NULL;
-
     insertAtTail(head, 1);
+    insertAtTail(head, 2);
     insertAtTail(head, 3);
-    insertAtTail(head, 5);
-    insertAtTail(head, 7);
+    insertAtTail(head, 4);
+    insertAtTail(head, 6);
+    insertAtTail(head, 8);
     insertAtTail(head, 10);
 
     display(head);
