6f64345f3441f180cec4c66be5a4c5d500383352
VivekWesley <vivekjohnwesley@gmail.com>
2021-06-01 22:48:12 +0530

    ADD merge sort practice

--- ALGO/mergeSort.cpp
+++ ALGO/mergeSort.cpp
@@ -467,6 +467,101 @@
 // }
 
 // #5
+// #include <iostream>
+// using namespace std;
+
+// void merge(int arr[], int l, int mid, int r)
+// {
+//     int n1 = mid - l + 1;
+//     int n2 = r - mid;
+
+//     int a[n1];
+//     int b[n2];
+
+//     for (int i = 0; i < n1; i++)
+//     {
+//         a[i] = arr[l + i];
+//     }
+
+//     for (int i = 0; i < n2; i++)
+//     {
+//         b[i] = arr[mid + 1 + i];
+//     }
+
+//     int i = 0;
+//     int j = 0;
+//     int k = l;
+
+//     while (i < n1 && j < n2)
+//     {
+//         if (a[i] < b[j])
+//         {
+//             arr[k] = a[i];
+//             k++;
+//             i++;
+//         }
+//         else
+//         {
+//             arr[k] = b[j];
+//             k++;
+//             j++;
+//         }
+//     }
+
+//     while (i < n1)
+//     {
+//         arr[k] = a[i];
+//         k++;
+//         i++;
+//     }
+
+//     while (j < n2)
+//     {
+//         arr[k] = b[j];
+//         k++;
+//         j++;
+//     }
+// }
+
+// void mergeSort(int arr[], int l, int r)
+// {
+//     if (l < r)
+//     {
+//         int mid = l + (r - l) / 2;
+
+//         mergeSort(arr, l, mid);
+//         mergeSort(arr, mid + 1, r);
+
+//         merge(arr, l, mid, r);
+//     }
+// }
+
+// int main()
+// {
+//     int n;
+//     cout << "enter size of the array: " << endl;
+//     cin >> n;
+
+//     int arr[n];
+//     cout << "enter array elements: " << endl;
+//     for (int i = 0; i < n; i++)
+//     {
+//         cin >> arr[i];
+//     }
+
+//     mergeSort(arr, 0, n - 1);
+
+//     cout << "sorted array : " << endl;
+//     for (int i = 0; i < n; i++)
+//     {
+//         cout << arr[i] << " ";
+//     }
+//     cout << endl;
+
+//     return 0;
+// }
+
+// #6
 #include <iostream>
 using namespace std;
 
@@ -539,7 +634,8 @@ void mergeSort(int arr[], int l, int r)
 int main()
 {
     int n;
-    cout << "enter size of the array: " << endl;
+
+    cout << "enter array size: " << endl;
     cin >> n;
 
     int arr[n];
@@ -551,7 +647,7 @@ int main()
 
     mergeSort(arr, 0, n - 1);
 
-    cout << "sorted array : " << endl;
+    cout << "sorted array: " << endl;
     for (int i = 0; i < n; i++)
     {
         cout << arr[i] << " ";
